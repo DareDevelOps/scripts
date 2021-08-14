7dec1666dfed706f2610edf7f2389b36f7da9c03
Drew Burt <burtd667@gmail.com>
2021-08-14 13:05:55 -0400

    modified new-ghRepo.ps1; added more git commands.

--- new-ghRepo.ps1
+++ new-ghRepo.ps1
@@ -8,11 +8,17 @@ Automatically create a generic Github Repo.
 #>
 
 $repoName = read-host -Prompt "Provide a Repo name"
+$repoURI = Read-Host -Prompt 'Please enter the your Github repository name'
+
 
 New-Item -Path . -Name $repoName -ItemType Directory
 Set-Location -Path $repoName
 
 git init 
 gh repo create $repoName -y -l MIT --public
+git remote add $repoName main $repoURI + \$repoName
 
 new-item -Path . -Name README.MD -ItemType File
+
+git add *
+git commit -m 'Initial commit.'
